20e71113309bcce91addda94c70bca1ab8b7bb8f
mmlaasesmed-denny <sb@mmlaasesmed.dk>
2026-08-07 13:57:19 +0100

    Fix React.Fragment ReferenceError in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Section, PageElement, SiteTheme, ElementStyles, ElementType, Column } from './types';
 import { TEMPLATES, COLOR_THEMES } from './templates';
 import Canvas from './components/Canvas';
